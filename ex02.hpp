48bd8b1b73025498909b06da5ba80edb1c82579b
macoyshev <make.oynoshev@gmail.com>
2022-03-07 16:43:30 +0300

    .

--- ex02.hpp
+++ ex02.hpp
@@ -1,21 +1,29 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-
+template <typename T>
 public class Calculator {
 private:
-    int num1, num2;
+    T num1, num2;
 
 public:
-    Calculator(int n1, int n2) {
+    Calculator(T n1, T n2) {
         num1 = n1;
         num2 = n2;
     }
 
-    int add() { return 0; /*your code here*/ }
-    int subtract() { /*your code here*/ }
-    int multiply() { /*your code here*/ }
-    int divide() { /*your code here*/ }
+    int add() {
+        return n1 * n2;
+    }
+    int subtract() {
+        return n1 - n2;
+    }
+    int multiply() {
+        return n1 * n2;
+    }
+    int divide() {
+        return n1 / n2
+    }
 };
 
 
