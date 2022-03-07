76737f3f7a49c65426eb7b4eb7ded38fa53d2e6f
macoyshev <make.oynoshev@gmail.com>
2022-03-07 17:12:26 +0300

    asd

--- ex01.hpp
+++ ex01.hpp
@@ -1,9 +1,12 @@
-
+#include <math.h>
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename T>
 void multiples (T& sum, T x, T n)
 {
-    sum = x * n;
+    sum += 1;
+    for (int i = 1; i <= n; i++) {
+        sum += i * x;
+    }
 }
